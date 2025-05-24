740137732aefbeeff8ffef7f54afde2743d8c969
omis-logistic <myorder.onlineshopping@gmail.com>
2025-05-24 18:51:44 +0800

    app.js
    update proxy url

--- scripts/app.js
+++ scripts/app.js
@@ -2,7 +2,7 @@
 // ================= CONFIGURATION =================
 const CONFIG = {
   GAS_URL: 'https://script.google.com/macros/s/AKfycbzSaW6sBX28XHzHt9mxGxb86EM8fOqE7k9kGwHfNh7yraSXeKO3oSHm1bSJHu0AGITe/exec',
-  PROXY_URL: 'https://script.google.com/macros/s/AKfycbz3a930Q8zfWsfmzxCLZxOjmaSN66udwfQgkyDryLXafMwakOUCXrFruaCrK8Aa4woI/exec',
+  PROXY_URL: 'https://script.google.com/macros/s/AKfycbz-IVd14yhd4HH-yXahZlRB2OEbbglS6G1M6cqtZqv0G0r1zwn0aSJT92ZqX0fxi1Sk/exec',
   SESSION_TIMEOUT: 3600,
   MAX_FILE_SIZE: 5 * 1024 * 1024,
   ALLOWED_FILE_TYPES: ['image/jpeg', 'image/png', 'application/pdf'],
